aa1d11f122e481a3b3fe7b7bad0405b579c50716
Sibro <shibo960830@163.com>
2019-09-24 10:25:48 +0800

    2019/09/24  001

--- src/main/java/com/sibro/community/controller/AuthorizeController.java
+++ src/main/java/com/sibro/community/controller/AuthorizeController.java
@@ -1,8 +1,10 @@
 package com.sibro.community.controller;
 
 import com.sibro.community.dto.AccessTokenDTO;
+import com.sibro.community.dto.GitHubUser;
 import com.sibro.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,16 +14,26 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String client_id;
+
+    @Value("${github.client.secret}")
+    private String client_secret;
+
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,@RequestParam("state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("d14b35c68273cd81ca22");
-        accessTokenDTO.setClient_secret("09593255ffb569886396ef8514fe1563be1584e6");
+        accessTokenDTO.setClient_id(client_id);
+        accessTokenDTO.setClient_secret(client_secret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/calback");
-        gitHubProvider.getAccessToken(accessTokenDTO);
+        accessTokenDTO.setRedirect_uri(redirect_uri);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GitHubUser user = gitHubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
--- src/main/java/com/sibro/community/provider/GitHubProvider.java
+++ src/main/java/com/sibro/community/provider/GitHubProvider.java
@@ -2,6 +2,7 @@ package com.sibro.community.provider;
 
 import com.alibaba.fastjson.JSON;
 import com.sibro.community.dto.AccessTokenDTO;
+import com.sibro.community.dto.GitHubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -9,18 +10,39 @@ import java.io.IOException;
 
 @Component
 public class GitHubProvider {
+    /*
+    * 得到access_token
+    * */
     public String getAccessToken(AccessTokenDTO accessTokenDTO){
-        MediaType JSON
-                = MediaType.get("application/json; charset=utf-8");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(JSON, com.alibaba.fastjson.JSON.toJSONString(accessTokenDTO));
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        } catch (IOException e) {
+            String responseStr = response.body().string();
+            return (responseStr.split("&"))[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /*
+    * 得到登录用户信息
+    * */
+    public GitHubUser getUser(String access_token){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?"+access_token)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String responseStr = response.body().string();
+            GitHubUser gitHubUser = JSON.parseObject(responseStr, GitHubUser.class);
+            return gitHubUser;
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
